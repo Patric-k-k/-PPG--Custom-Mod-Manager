8174ebb0b0f7ff0e0429716e66f7548aa43068d6
Patric-k-k <104700188+Patric-k-k@users.noreply.github.com>
2025-05-20 16:01:09 -0400

    You can now view mods, also if they are active or not.

--- items.php
+++ items.php
@@ -7,13 +7,17 @@ foreach ($extact as $act) {
 }
 
 # Display mods, using extact to show enabled/disabled mods
+
+# So, you know the part in readme.md that I was talking about the bug? yeah, thats in the lower
+# chunk of code. In line 18, it commonly doesn't return a array. There could be more going on here.
+# If statements that exists just because of this are marked "cI1"
 foreach (new DirectoryIterator($_GET["path"]) as $fileInfo) {
     if($fileInfo->isDot()) continue;
     $path = $fileInfo->getPathname();
     if (file_exists("$path/mod.json")) {
         $data = json_decode(file_get_contents("$path/mod.json"),true);
-        if (is_array($data)) {
-            if (array_key_exists("Name",$data)) {
+        if (is_array($data)) { #cI1
+            if (array_key_exists("Name",$data)) { #cI1
                 if (in_array($fileInfo->getFilename(),$extact)) {
                     $color = "green";
                 } else {
---
+++ readme.md
@@ -0,0 +1,9 @@
+I hated the built in one too much-- mainly because it didn't have collections. Neither does this one, for now.
+## Implemented
+- Viewing mods
+- Shows if the mod is active
+- For some reason breaks on random mods? check items.php if you feel like investigating the issue.
+## Planned Features
+- Collections
+- Toggleing Mods
+###### oh wait yeah its a mod manager thats about it.
